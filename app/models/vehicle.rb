27b6390e9a05d8d47640e7ec0a99c011667a1e7f
Wonjin Cho <cwonjin11@gmail.com>
2021-04-06 13:24:55 -0400

    vecicle model validation coded

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,9 @@
 
 class Vehicle < ActiveRecord::Base
     belongs_to :user
-    validates :brand, :style, :color, :year, :vin_number, :model, :price, presence: true
+    validates :brand, :style, :color, :year, :model, :price, :vin_number, presence: true
+    validates :vin_number, length: { is: 17 }
+    validates :year, numericality: { only_integer: true }
+    validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+   
 end
