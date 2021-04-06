fa1604de9f95ef03815acf8caa6a2aa6f0a4517c
Wonjin Cho <cwonjin11@gmail.com>
2021-04-06 13:23:49 -0400

    user validation coded

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
     has_many :vehicles
     has_secure_password
+
+    validates_presence_of :username, :email, :password
+    validates_uniqueness_of :username, :email
+    
+
 end
