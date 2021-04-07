5d4e7515fa13f210ace80887b3a4d0dd5afe4533
Wonjin Cho <cwonjin11@gmail.com>
2021-04-06 21:27:07 -0400

    sset password seed data as password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
-
 User.destroy_all
 Vehicle.destroy_all
 
 10.times do
-    User.create(username: Faker::Alphanumeric.alphanumeric(number: 10), email: Faker::Internet.email, password_digest: Faker::Beer.brand)
+    User.create!(username: Faker::Internet.username, email: Faker::Internet.email, password: "password")
 end
 
 
 200.times do
-    Vehicle.create(brand:Faker::Vehicle.make , style: Faker::Vehicle.car_type, color: Faker::Vehicle.color, year: Faker::Vehicle.year, vin_number: Faker::Vehicle.vin, user_id: rand(1..10), model:Faker::Vehicle.model, price: rand(11000.00..99900.00))
+    Vehicle.create!(brand:Faker::Vehicle.make , style: Faker::Vehicle.car_type, color: Faker::Vehicle.color, year: Faker::Vehicle.year, vin_number: Faker::Vehicle.vin, user_id: rand(1..10), model:Faker::Vehicle.model, price: rand(11000.00..99900.00))
 end
