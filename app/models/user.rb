eff6eeff65ad99a1ff35cf07f5ec23dd0917bad0
Wonjin Cho <cwonjin11@gmail.com>
2021-04-12 13:37:41 -0400

    deleted password from the 'validation precence of' line. It is already required with bcrypt gem so do not need here.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     #loads bcrypt gem to make the password salted and hased.
 
 
-    validates_presence_of :username, :email, :password
+    validates_presence_of :username, :email #, :password => password is required with bcrypt gem as well already.
     validates_uniqueness_of :username, :email 
 
 
