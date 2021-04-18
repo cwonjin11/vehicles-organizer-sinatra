24179237aa1201b0b1a46d66a2960cea28afdad1
Wonjin Cho <cwonjin11@gmail.com>
2021-04-18 01:06:06 -0400

    minor change; line clean up. Helpers box test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,13 @@ class ApplicationController < Sinatra::Base
 
 
   helpers do #helper block tells sinatra that theses method should be available anywhere
-    def logged_in? # "?" returns true or false 
+    def logged_in? # "?" returns true or false   # check to see if a user is logged in
       !!session[:user_id]
+      # @user ||= User.find_by(id: session[:user_id])
       #This method should return true if the user_id is in the session hash and false if not.
     end
 
-    def current_user
+    def current_user # method to keep track of logged in user
       @current_user = User.find(session[:user_id]) 
       # same as @current_user ||= User.find_by(id: session[user_id])
       #This method should use the user_id from the session hash to find the user in the database and return that user
